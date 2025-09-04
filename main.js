6d5490aeec862bce14c1d1b39dbfb9111c0fa533
Ernest T Javachava <ernestjavaz@gmail.com>
2025-09-04 13:31:34 +0200

    add author feature

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
+const btn=document.getElementById('btn');
+const qoutes=document.getElementById('qoutes') 
+const author=document.getElementById('author')
+btn.addEventListener('click', function(){
+async function getQuote(){
 
-
-const btn=document.getElementById("btn");
-
-
-btn.addEventListener('click',function(){
- 
- async function getquote() {
-    let quotes=document.getElementById("qoutes");
     try{
-    const response= await fetch('https://api.api-ninjas.com/v1/quotes',{
+    const response=await fetch('https://api.api-ninjas.com/v1/quotes',{
         method:"GET",
         headers:{
-            'X-Api-Key': 'KSJj7SzS9ZHO+EuJ0pUh2g==PhhzT3ab3jlXDKEE',
-            'Content-type':'application/json'
+            'X-Api-Key':'KSJj7SzS9ZHO+EuJ0pUh2g==PhhzT3ab3jlXDKEE',
+            'Content-Type':'application/json'
         }
     })
-     if(!response.ok){
-    throw new Error(`error http ${response.status}`)
- }
- const data=await response.json();
-
-console.log(quotes)
- quotes.textContent=data[0].quote
- }
-catch(error){
-    console.error('error' ,error.message)
-}
- 
- }
-getquote();
-})
-
-
     
+    if(!response.ok){
+        throw new Error(`error: ${response.status}`)
+       return 
+        
+    }
+    const data=await response.json();
+    qoutes.innerText=data[0].quote;
+    author.innerText=data[0].author;
+
+    if(data[0].quote=="" || data[0].author==""){
+ alert('could not find any quotes');
+ return;
+
+    }
+       
+    }
+catch(error)   {
+console.error("error:",error.message)
+}
+}
+
+getQuote();
+})
