9aa2b7a891b5401de5d7cd4ba3fc4ca6a38c319a
Ernest T Javachava <ernestjavaz@gmail.com>
2025-09-04 13:39:17 +0200

    add comments

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// variable declaration
 const btn=document.getElementById('btn');
 const qoutes=document.getElementById('qoutes') 
 const author=document.getElementById('author')
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(){  //adding an eventlistener btn
 async function getQuote(){
 
-    try{
+    try{        //a try catch block to catch an error if the fetch fails
     const response=await fetch('https://api.api-ninjas.com/v1/quotes',{
         method:"GET",
         headers:{
@@ -12,27 +13,27 @@ async function getQuote(){
             'Content-Type':'application/json'
         }
     })
-    
-    if(!response.ok){
+    //if the response is not ok it should throw in that error and stop ecevuting
+    if(!response.ok){ 
         throw new Error(`error: ${response.status}`)
        return 
         
     }
-    const data=await response.json();
+    const data=await response.json(); //if request is ok  get the response as a json file
     qoutes.innerText=data[0].quote;
     author.innerText=data[0].author;
 
     if(data[0].quote=="" || data[0].author==""){
- alert('could not find any quotes');
+ alert('could not find any quotes'); // if data returns nothing then alert user with this error and return
  return;
 
     }
        
     }
 catch(error)   {
-console.error("error:",error.message)
+console.error("error:",error.message)  
 }
 }
 
-getQuote();
+getQuote(); //call the getQoute function
 })
